c282eb4449b64850b36caf0647091472850239a3
Patrick Richey <patrick_richey@hotmail.com>
2015-05-13 13:23:07 -0400

    commit at 1:22

---
+++ Lesson 2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lesson_2.Global" Language="C#" %>
---
+++ Lesson 2/default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lesson_2
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if the form wasn't submitted
+            if(!IsPostBack)
+            {
+                lblMessage.Text = "";
+            }
+        }
+
+        protected void btnCopyText_Click(object sender, EventArgs e)
+        {
+            //copy the text box to the label
+            lblMessage.Text = txtMessage.Text;
+            btnCopyText.Enabled = false;
+        }
+
+        protected void btnShowSelections_Click(object sender, EventArgs e)
+        {
+            //clear out the previous label text first
+            lblToppings.Text = "";
+            //loop through the list of checkboxes
+            foreach (ListItem topping in cblToppings.Items)
+            {
+                //check if the current topping is checked or not
+                if (topping.Selected)
+                {
+                    lblToppings.Text += topping.Text + " ";
+                }
+            }
+            //display the selected size
+            lblSize.Text = ddlSize.SelectedItem.Text;
+        }
+    }
+}
